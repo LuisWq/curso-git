d15e1a866688e914d3b4f509d0b0efdde5b7b161
Luis Gustavo <luisgustavo.ferreiradasilva99@yahoo.com>
2018-01-15 19:30:27 -0200

    Acrescentada a pergunta sobre como voce esta hoje?

--- Aula56/Program.cs
+++ Aula56/Program.cs
@@ -12,6 +12,7 @@ namespace Aula56
         {
             Console.WriteLine("Olá Mundo!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está hoje?");
 
             Console.ReadKey();
             
