523d4cc0c3e9500cdb2400606660cb8ae91bb9af
Luis Gustavo <luisgustavo.ferreiradasilva99@yahoo.com>
2018-01-15 19:00:04 -0200

    Projeto iniciado, mostrando a mensagem ola mundo

---
+++ Aula56/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+
+            Console.ReadKey();
+            Console.WriteLine();
+
+
+            Console.WriteLine("Tchau Mundo!");
+
+            Console.ReadLine();
+        }
+    }
+}
